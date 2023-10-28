bee27959e98035e851efc6315b38631f0fd59aaf
imDzed <mhafidz.hidayat@gmail.com>
2023-10-28 19:42:45 +0700

    Starting project Tokoku

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===================================")
+	fmt.Println("\tWelcome TO Tokoku")
+	fmt.Println("===================================")
+	fmt.Println("\tComing soon .. !!!")
+	fmt.Println("===================================")
+}
