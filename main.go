183ed629ddbabfe7589d650d5d52d0d360964454
aditya-tri <adityatrisaktisilalahi@gmail.com>
2023-11-01 05:59:33 +0700

    adding menu and fitur register

--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"tokoku/auth"
 	"tokoku/config"
+	"tokoku/model"
 )
 
 func main() {
+	var inputMenu int
 	db, err := config.InitDB()
 	if err != nil {
-		fmt.Println(db)
+		fmt.Println(db, err.Error())
+		return
+	}
+
+	db.AutoMigrate(&model.User{})
+
+	var auth = auth.AuthSystem{DB: db}
+
+	for {
+		fmt.Println("1. Login")
+		fmt.Println("2. Register")
+		fmt.Println("99. Exit")
+		fmt.Print("Masukkan pilihan:")
+		fmt.Scanln(&inputMenu)
+		switch inputMenu {
+		case 2:
+			result, permit := auth.Register()
+			if permit {
+				fmt.Println(result)
+			}
+		case 99:
+			fmt.Println("Thank you....")
+			return
+		default:
+
+		}
 	}
 
 }
